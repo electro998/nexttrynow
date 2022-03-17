2acf4aac139a3bd25fc2bc927867079fd1da8828
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 00:05:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* you can add metadata here, for all pages */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+<meta charset="UTF-8"/>
+<title>Positive</title>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="TEST"/>
+<meta property="og:description" content=" ..."/>
+<meta property="og:url" content=""/>
+<meta property="og:site_name" content=""/>
+<meta property="article:section" content="Animal"/>
+<meta property="og:image" content="https://natureanimalrescue.vercel.app/wp-content/uploads/2021/06/cover2-32-1.png"/>
+<meta property="og:image:alt" content="IMAGE ALT"/><style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
       <Component {...pageProps} />
     </>
