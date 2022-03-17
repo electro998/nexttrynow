8b83f92e9a8e6e13dc2a5cd18b4aa7eb33d62154
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 00:45:17 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,9 @@ import Link from "next/link";
 import '../styles/globals.css'
 import {getDate} from "../utils/utils";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
+export default function PostPage({post}){
+    return (
+  <Head>
   <title>{post.title.rendered}</title>
 <meta property="og:locale" content="en_US"/>
 <meta property="og:type" content="article"/>
@@ -17,8 +16,5 @@ export default function MyApp({ Component, pageProps }) {
 <meta property="og:image:alt" content="IMAGE ALT"/>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
-      <Component {...pageProps} />
-    </>
-  )
-    
+    )
 }
