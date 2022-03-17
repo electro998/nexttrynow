c067b4a442888d18779ecd741737decd21d6708e
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 00:47:54 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import Link from "next/link";
-import '../styles/globals.css'
-import {getDate} from "../utils/utils";
 
-export default function PostPage({post}){
-    return (
-  <Head>
-  <title>{post.title.rendered}</title>
-<meta property="og:locale" content="en_US"/>
-<meta property="og:type" content="article"/>
-<meta property="og:title" content="" />
-<meta property="og:url" content=""/>
-<meta property="og:site_name" content=""/>
-<meta property="article:section" content="Animal"/>
-<meta property="og:image:alt" content="IMAGE ALT"/>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        { 
+<meta property="og:locale" content="en_US"/>}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
-    )
+      <Component {...pageProps} />
+    </>
+  )
+    
 }
+
+export default MyApp
