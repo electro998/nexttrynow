2119e34f7aba9224902293b178f5bd83fd351ca8
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 13:00:02 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-    async redirects() {
-    return [
-      {
-        source: '/posts',
-        destination: 'https://lajmejavore.info/',
-        permanent: false,
-        basePath: false
-      },
-    ]
-},
  images: {
         //enter the domain or subdomain where you have WordPress installed 
       domains: ['positivityminds.com'],
