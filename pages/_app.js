ca514a9ae2f22d1ccfd3ae516728e13cc7609658
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:22:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,18 @@ import '../styles/globals.css'
 import Link from "next/link";
 import Image from "next/image";
 
-function MyApp({Component, pageProps, post}) {
+export default function MyApp({Component, pageProps, post}) {
+
   return (
     <>
-    <div>
       <Head>
 
         <title>{post.title.rendered}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
-      </Head></div>
+      </Head><
       <Component {...pageProps} />
     </>
   )
     
 }
 
-export default MyApp
