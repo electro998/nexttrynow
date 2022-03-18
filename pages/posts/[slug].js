5ff4fcb05d69d89e90fe54e6e624ea14adf87efb
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-19 00:22:35 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -6,7 +6,6 @@ export default function PostPage({post, featuredMedia}) {
     return (
         <>
         <Head>
-   </Head>
 <title>{post.title.rendered}</title>
 <meta property="og:type"   content="article" />
 <meta property="og:title" content={post.title.rendered} />
@@ -24,8 +23,9 @@ export default function PostPage({post, featuredMedia}) {
 <meta property="og:locale" content="en_US" />
 <meta property="article:publisher" content="https://www.facebook.com/animmalslove/" />
 <meta name="keywords" content="cats, cat, kitty, kitten , puppy, dog, dogs, animals, funny cats, funny kitten, amazing cats, amazing animals, home cats, home kitten, Animated GIF" />
- 
-        <div className="container pt-5">
+ </Head>
+
+       <div className="container pt-5">
         </div>
  </>
     )
