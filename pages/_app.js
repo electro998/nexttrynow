3d0df4a739c4ad5bfde60e42d5d0943a525defe5
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:01:39 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
 import Head from 'next/head'
 import React from 'react'
-import { Helmet } from "react-helmet";
 import '../styles/globals.css'
 
 export default function ExamplePage(props) {
     const { page } = props;
     return (
         <div class={"page"}>
-            <Helmet>
+            <Head>
                 <title>{"This is a page"}</title>
-            </Helmet>
+            </HEAD>
             <h1 class={"page-title"}>{page.title.rendered}</h1>
             <div class={"page-content"} dangerouslySetInnerHTML={{ __html: props.page.content.rendered }}/>
         </div>
