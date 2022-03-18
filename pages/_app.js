919d4850510caaedf5bc8acd2b1367e5e0615fa4
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:18:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,12 @@ import Image from "next/image";
 function MyApp({ Component, pageProps, post }) {
   return (
     <>
+    <div>
       <Head>
 
         <title>${post.title.rendered}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
-      </Head>
+      </Head></div>
       <Component {...pageProps} />
     </>
   )
