5909034532b2377e48c3f37948dd5e01d1c4dc88
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:17:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
-
+import {getDate} from "../utils/utils";
 import '../styles/globals.css'
+import Link from "next/link";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps, post }) {
   return (
     <>
       <Head>
 
-        { <title>{post.title.rendered}</title>}
+        <title>{post.title.rendered}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
       <Component {...pageProps} />
