b1dfb6a9960da2487429e1e2d7050d87612b120b
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:19:20 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps, post }) {
     <div>
       <Head>
 
-        <title>${post.title.rendered}</title>
+        <title>{post.title.rendered}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head></div>
       <Component {...pageProps} />
