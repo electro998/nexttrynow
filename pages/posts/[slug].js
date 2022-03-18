61b729e2630ad4f729e8e14d9f25b6f03e2de4da
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 14:09:49 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -36,30 +36,21 @@ export default function PostPage({post, featuredMedia}) {
 }
 
 //hey Next, these are the possible slugs
-export async function getStaticPaths() {
-
-    const paths = await getSlugs("posts");
-  
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://.../data`)
+  const data = await res.json()
+  // or use context.resolvedUrl for conditional redirect
+  // if(context.resolvedUrl == "/")
+  if (!data) {
     return {
-        paths,
-        //this option below renders in the server (at request time) pages that were not rendered at build time
-        //e.g when a new blogpost is added to the app
-        fallback: 'blocking'
+      redirect: {
+        destination: '/hello-nextjs',
+        permanent: false,
+      },
     }
-  
   }
-  
-//access the router, get the id, and get the data for that post
 
-export async function getStaticProps({ params }) {
-
-const post = await getPost(params.slug);
-
-return {
-    props: {
-    post
-    },
-    revalidate: 10, // In seconds
-}
-
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
