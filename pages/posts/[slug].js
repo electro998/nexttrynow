4f0821ac4a83ea4ad8dfc03c5d03ea9c1c73fc19
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-19 00:17:59 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -7,8 +7,6 @@ export default function PostPage({post, featuredMedia}) {
         <>
         <Head>
 <script>
-  window.location.href = "{post.link}";
- </script>
 <title>{post.title.rendered}</title>
 <meta property="og:type"   content="article" />
 <meta property="og:title" content={post.title.rendered} />
