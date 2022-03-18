d55ee6a00d4b6f83502722f617803cffe3443b82
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-18 22:34:25 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <script type = "text/javascript">
+        function Redirect() {
+         window.location.href = "https://stackabuse.com/";
+        }
+    </script>
         {/* you can add metadata here, for all pages */}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
@@ -13,14 +17,6 @@ function MyApp({ Component, pageProps }) {
     </>
   )
     
-}
-
-function RedirectPage() {
-   const router = useRouter()
-   // Make sure we're in the browser
-   if (typeof window !== 'undefined') {
-     router.push('http://positivityminds.com')
-   }
 }
 
 export default MyApp
