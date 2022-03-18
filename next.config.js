854a1d55d787799717a25cb8a1c6a20f99027888
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-18 23:29:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/posts/:slug*',
-        destination: '{post.link}',
-        permanent: true,
-      },
-    ]
-  },
-    
   images: {
       domains: ['positivityminds.com'],
     },  
