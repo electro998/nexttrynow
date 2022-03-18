ca8c0dd0f157edc1766c65cd9670767564dcb489
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 13:47:58 +0100

    Update [code].jsx

--- pages/posts/[code].jsx
+++ pages/posts/[code].jsx
@@ -3,3 +3,9 @@ const RedirectURL = () => {
 }
 
 export default RedirectURL;
+
+export const getServerSideProps = async (context) => {
+  const { res } =  context;
+  res.writeHead(301, { location: "https://google.com" } );
+  res.end();
+}
