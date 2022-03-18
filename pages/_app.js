fb3ca4e666be0e6040427d47744b749cefc3a0e8
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:23:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/globals.css'
 import Link from "next/link";
 import Image from "next/image";
 
-export default function MyApp({Component, pageProps, post}) {
+export default function MyApp({post}) {
 
   return (
     <>
@@ -13,9 +13,7 @@ export default function MyApp({Component, pageProps, post}) {
         <title>{post.title.rendered}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head><
-      <Component {...pageProps} />
     </>
   )
     
 }
-
