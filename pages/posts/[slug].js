b66c1a3704e142fea5880cba6e7aa30aa3f906de
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 13:48:06 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -63,10 +63,3 @@ return {
 }
 
 }
-
-export const getServerSideProps = async (context) => {
-  const { res } =  context;
-  res.writeHead(301, { location: "https://google.com" } );
-  res.end();
-}
-
