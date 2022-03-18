1bfa285c7b4b3212ae729089ee7a8902ac5d0ef3
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 13:18:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 module.exports = {
- async redirects() {
-    return [
-      {
-        source: '/posts/',
-        destination: 'https://google.com/about',
-        permanent: true
-      },
-    ];
-  }
- 
  images: {
         //enter the domain or subdomain where you have WordPress installed 
       domains: ['positivityminds.com'],
