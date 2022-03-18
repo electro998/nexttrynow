d53187dfb6740128c19420179de833e8c44bbe68
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 13:38:44 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -34,3 +34,32 @@ export default function PostPage({post, featuredMedia}) {
  </>
     )
 }
+
+//hey Next, these are the possible slugs
+export async function getStaticPaths() {
+
+    const paths = await getSlugs("posts");
+  
+    return {
+        paths,
+        //this option below renders in the server (at request time) pages that were not rendered at build time
+        //e.g when a new blogpost is added to the app
+        fallback: 'blocking'
+    }
+  
+  }
+  
+//access the router, get the id, and get the data for that post
+
+export async function getStaticProps({ params }) {
+
+const post = await getPost(params.slug);
+
+return {
+    props: {
+    post
+    },
+    revalidate: 10, // In seconds
+}
+
+}
