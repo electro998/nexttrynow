14b10eac1b5ab5935d98b09a527d812a048b98ea
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-18 22:34:55 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script type = "text/javascript">
-        function Redirect() {
-         window.location.href = "https://stackabuse.com/";
-        }
-    </script>
         {/* you can add metadata here, for all pages */}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
