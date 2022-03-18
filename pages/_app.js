9d828e30f4e4ad22e8ac752219bab5d8b42c60c9
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-18 01:33:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,18 @@
-import Link from "next/link";
-import Image from "next/image";
-//to use Image with an external url, add some config on next.config.js
-//for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
+import Head from 'next/head'
 
-import {getDate} from "../utils/utils";
+import '../styles/globals.css'
 
-export default function MyApp({post, featuredMedia}) {
-
-    return (
-        <div className="card mb-3" style={{maxWidth: "540px"}}>
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <Link href={`/posts/${post.slug}`}>
-                        <a>
-                            <Image src={featuredMedia["media_details"].sizes.medium["source_url"]} width={180} height={120} alt={featuredMedia["alt_text"]}/>
-                        </a>
-                    </Link>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">{post.title.rendered}</h5>
-                        <div className="card-text" dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}></div>
-                        <p className="card-text"><small className="text-muted">On {getDate(post.modified)}</small></p>
-                        <Link href={`/posts/${post.slug}`}>
-                            <a className="btn btn-primary">See more</a>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* you can add metadata here, for all pages */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+    
 }
+
+export default MyApp
